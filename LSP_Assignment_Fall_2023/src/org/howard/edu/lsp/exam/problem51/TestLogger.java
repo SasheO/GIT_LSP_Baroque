7c0a2c046baa54e185b89e55bd2d5617ddeba1cb
SasheO <76496417+SasheO@users.noreply.github.com>
2023-12-07 20:16:13 -0500

    Create TestLogger.java

---
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/exam/problem51/TestLogger.java
@@ -0,0 +1,24 @@
+package org.howard.edu.lsp.exam.problem51;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestLogger {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

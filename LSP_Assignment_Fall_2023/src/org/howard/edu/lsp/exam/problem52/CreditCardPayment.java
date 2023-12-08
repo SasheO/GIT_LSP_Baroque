5e62cba10141001ab5a365d727de4697acd3ee41
SasheO <76496417+SasheO@users.noreply.github.com>
2023-12-07 20:43:38 -0500

    added probelm 52 folder

---
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/exam/problem52/CreditCardPayment.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.exam.problem52;
+
+public class CreditCardPayment implements PaymentStrategy {
+
+	@Override
+	public void pay(double pay) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public CreditCardPayment(String creditCardNo) {
+		
+	}
+
+}
---
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/exam/problem52/PayPalPayment.java
@@ -0,0 +1,9 @@
+package org.howard.edu.lsp.exam.problem52;
+
+public class PayPalPayment {
+
+	public PayPalPayment(String emailAddress) {
+		// TODO Auto-generated constructor stub
+	}
+
+}

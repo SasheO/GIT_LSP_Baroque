0962ca5fd3fd6ad8baf6ee8895af4c624aa971bb
SasheO <76496417+SasheO@users.noreply.github.com>
2023-12-07 20:52:53 -0500

    Update ShoppingCart.java

--- LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/exam/problem52/ShoppingCart.java
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/exam/problem52/ShoppingCart.java
@@ -7,7 +7,9 @@ public class ShoppingCart {
 	public ShoppingCart() {
 		// TODO Auto-generated constructor stub
 	}
-	
+	public void setPaymentMethod(PaymentStrategy paymentStrategy) {
+		this.paymentStrategy = paymentStrategy;
+	}
 	public void checkout() {
 		
 	}
